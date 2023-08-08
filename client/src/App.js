401fc279cf13d4b49b08320f055c2c754aed2a18
AngadBatth <andyybatth@gmail.com>
2023-08-08 04:50:55 -0400

    update to src

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,16 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-// Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -31,7 +26,6 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -45,16 +39,13 @@ function App() {
           <Routes>
             <Route 
               path="/" 
-              element={<SearchBooks/>} 
-            />
+              element={<SearchBooks/>} />
             <Route 
               path="/saved" 
-              element={<SavedBooks/>} 
-            />
+              element={<SavedBooks/>} />
             <Route 
               path='*' 
-              element={<h1 className="display-2">Wrong page!</h1>}
-            />
+              element={<h1 className="display-2">Wrong page!</h1>}/>
           </Routes>
         </>
       </Router>
